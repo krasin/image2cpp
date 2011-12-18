0b7ce5b04d34fec63f7a8ebdf5ca695714491cad
Ivan Krasin <imkrasin@gmail.com>
2011-12-18 06:28:44 +0000

    Compact the image

--- image2cpp.go
+++ image2cpp.go
@@ -2,8 +2,76 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
+type Interval struct {
+	DataOff int
+	Off     int
+	Len     int
+}
+
+type Image struct {
+	Data    []byte
+	Sectors map[int][]Interval
+}
+
+func NewImage() *Image {
+	return &Image{
+		Sectors: make(map[int][]Interval),
+	}
+}
+
+func (im *Image) Add(sector int, data []byte) {
+	var intervals []Interval
+	ind := -1
+	off := 0
+	zcount := 0
+	for i, b := range data {
+		if b != 0 {
+			if zcount > 10 && ind >= 0 {
+				dataOff := len(im.Data)
+				im.Data = append(im.Data, data[off:ind+1]...)
+				intervals = append(intervals, Interval{dataOff, off, ind + 1 - off})
+				off = i
+			}
+			if zcount > 0 && ind == -1 {
+				off = i
+			}
+			ind = i
+			zcount = 0
+		} else {
+			zcount++
+		}
+
+	}
+	if ind == -1 {
+		return
+	}
+
+	dataOff := len(im.Data)
+	im.Data = append(im.Data, data[off:ind+1]...)
+	intervals = append(intervals, Interval{dataOff, off, ind + 1 - off})
+	im.Sectors[sector] = intervals
+}
+
 func main() {
-	fmt.Printf("lala\n")
+	im := NewImage()
+	buf := make([]byte, 512)
+	for i := 0; ; i++ {
+		_, err := io.ReadFull(os.Stdin, buf)
+		if err == io.EOF {
+			break
+		}
+		if err == io.ErrUnexpectedEOF {
+			log.Fatalf("Unexpected EOF from input stream")
+		}
+		if err != nil {
+			log.Fatalf("ReadFull: %v", err)
+		}
+		im.Add(i, buf)
+	}
+	fmt.Printf("Image: %v\n", im)
 }
